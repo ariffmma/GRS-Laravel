603fbd37938e69b55d0888d136f2facb938e44c0
anabata series <arifmaksum14@gmail.com>
2023-07-14 15:47:04 +0700

    projek detail

--- resources/views/frontend/Project-Detail.blade.php
+++ resources/views/frontend/Project-Detail.blade.php
@@ -13,7 +13,8 @@ sebelum </body>
 		<section class="u-clearfix u-image u-valign-middle u-section-1" id="sec-0dda" data-image-width="1920" data-image-height="1080"></section>
 		<section class="u-align-center u-clearfix u-section-2" id="sec-e222">
 			<div class="u-clearfix u-sheet u-sheet-1">
-				<div class="u-carousel u-carousel-fade u-expanded-width u-gallery u-gallery-slider u-layout-carousel u-lightbox u-no-transition u-show-text-on-hover u-thumbnails-position-right u-gallery-1" id="carousel-5851" data-interval="2000" data-u-ride="carousel">
+				{{-- <div class="u-carousel u-carousel-fade u-expanded-width u-gallery u-gallery-slider u-layout-carousel u-lightbox u-no-transition u-show-text-on-hover u-thumbnails-position-right u-gallery-1" id="carousel-5851" data-interval="2000" data-u-ride="carousel"> --}}
+				<div class="u-carousel u-expanded-width u-gallery u-gallery-slider u-layout-carousel u-lightbox u-no-transition u-show-text-on-hover u-thumbnails-position-right u-gallery-1" id="carousel-5851" data-interval="2000" data-u-ride="carousel">
 					<ol class="u-absolute-hcenter u-carousel-indicators u-carousel-indicators-1">
 						<li data-u-target="#carousel-5851" data-u-slide-to="0" class="u-active u-grey-70 u-shape-circle" style="width: 10px; height: 10px;"></li>
 						<li data-u-target="#carousel-5851" data-u-slide-to="1" class="u-grey-70 u-shape-circle" style="width: 10px; height: 10px;"></li>
@@ -66,32 +67,24 @@ sebelum </body>
 					<a class="u-absolute-vcenter u-carousel-control u-carousel-control-prev u-custom-color-3 u-icon-circle u-opacity u-opacity-70 u-spacing-10 u-text-white u-carousel-control-1" href="#carousel-5851" role="button" data-u-slide="prev">
 						<span aria-hidden="true">
 							<svg viewBox="0 0 451.847 451.847">
-								<path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0
-c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744
-c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"></path>
+								<path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"></path>
 							</svg>
 						</span>
 						<span class="sr-only">
 							<svg viewBox="0 0 451.847 451.847">
-								<path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0
-c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744
-c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"></path>
+								<path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"></path>
 							</svg>
 						</span>
 					</a>
 					<a class="u-absolute-vcenter u-carousel-control u-carousel-control-next u-custom-color-3 u-icon-circle u-opacity u-opacity-70 u-spacing-10 u-text-white u-carousel-control-2" href="#carousel-5851" role="button" data-u-slide="next">
 						<span aria-hidden="true">
 							<svg viewBox="0 0 451.846 451.847">
-								<path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744
-L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284
-c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"></path>
+								<path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"></path>
 							</svg>
 						</span>
 						<span class="sr-only">
 							<svg viewBox="0 0 451.846 451.847">
-								<path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744
-L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284
-c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"></path>
+								<path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"></path>
 							</svg>
 						</span>
 					</a>
@@ -104,21 +97,21 @@ c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,24
 				<h1 class="u-custom-font u-text u-text-1">{{ GoogleTranslate::trans($project->title, app()->getLocale()) }}</h1>
 				<div class="u-clearfix u-expanded-width u-gutter-24 u-layout-wrap u-layout-wrap-1">
 					<div class="row mt-3">
-							<div class="col-md-9 col-12 mb-3">
+							<div class="col-md-9 col-12 mb-2">
 								{!! $project['details'] !!}    
 								
 							</div>
-							<div class="col-md-3 col-12">
-									<div id="map" style="z-index:1;"></div>
-									<h1 class="u-custom-font u-text u-text-5">PT Global Rancang Selaras</h1>
+							<div class="col-md-3 col-12 mb-5">
+									<div id="map" style="z-index:1; margin-top: 8px;"></div>
+									<h1 class="u-custom-font u-text u-text-6">PT Global Rancang Selaras</h1>
 									<h1 class="u-custom-font u-text u-text-6">{{ GoogleTranslate::trans('Lokasi', app()->getLocale()) }}</h1>
 									<h1 class="u-custom-font u-text u-text-7">{{ $project->location }}</h1>
-									<h1 class="u-custom-font u-text u-text-8">{{ GoogleTranslate::trans('Skala', app()->getLocale()) }}</h1>
-									<h1 class="u-custom-font u-text u-text-9">{{ $project->scale }}</h1>
-									<h1 class="u-custom-font u-text u-text-10">{{ GoogleTranslate::trans('Tahun Proyek', app()->getLocale()) }}</h1>
-									<h1 class="u-custom-font u-text u-text-11"><a href="{{route('project-year',$project->year)}}">{{ $project->year }}</a></h1>
-									<h1 class="u-custom-font u-text u-text-10">{{ GoogleTranslate::trans('Tahun Penyelesaian', app()->getLocale()) }}</h1>
-									<h1 class="u-custom-font u-text u-text-11"><a href="{{route('project-year',$project->year)}}">{{ $project->completion_year }}</a></h1>
+									<h1 class="u-custom-font u-text u-text-6">{{ GoogleTranslate::trans('Skala', app()->getLocale()) }}</h1>
+									<h1 class="u-custom-font u-text u-text-7">{{ $project->scale }}</h1>
+									<h1 class="u-custom-font u-text u-text-6">{{ GoogleTranslate::trans('Tahun Proyek', app()->getLocale()) }}</h1>
+									<h1 class="u-custom-font u-text u-text-7"><a href="{{route('project-year',$project->year)}}">{{ $project->year }}</a></h1>
+									<h1 class="u-custom-font u-text u-text-6">{{ GoogleTranslate::trans('Tahun Penyelesaian', app()->getLocale()) }}</h1>
+									<h1 class="u-custom-font u-text u-text-7"><a href="{{route('project-year',$project->year)}}">{{ $project->completion_year }}</a></h1>
 								
 							</div>
 					</div>
@@ -126,12 +119,12 @@ c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,24
 			</div>
 		</section>
 		
-		<section class="u-clearfix u-section-3 garis" id="sec-192c" >
+		<section class="u-clearfix u-section-3 garis"  style="margin-bottom: 10px; id="sec-192c" >
 			<div class="u-clearfix u-sheet u-valign-middle-sm u-valign-middle-xs u-sheet-1">
-			<hr>
+			    <div class="garis-horizontal">
 			</div>
 		</section>
-		<section class="u-clearfix u-section-5 mb-3" id="sec-1b37">
+		<section class="u-clearfix u-section-5 mb-5" id="sec-1b37">
 			<div class="u-clearfix u-sheet u-valign-middle-lg u-valign-middle-md u-valign-middle-sm u-valign-middle-xs u-sheet-1">
 				<h1 class="u-custom-font u-text u-text-1">{{ GoogleTranslate::trans('Proyek Terkait', app()->getLocale()) }}</h1>
 				<!--blog-->
@@ -171,32 +164,24 @@ c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,24
 					<a class="u-custom-color-3 u-gallery-nav u-gallery-nav-prev u-icon-circle u-opacity u-opacity-70 u-spacing-10 u-text-white u-gallery-nav-1" href="#" role="button">
 						<span aria-hidden="true">
 							<svg viewBox="0 0 451.847 451.847">
-								<path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0
-c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744
-c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"></path>
+								<path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"></path>
 							</svg>
 						</span>
 						<span class="sr-only">
 							<svg viewBox="0 0 451.847 451.847">
-								<path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0
-c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744
-c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"></path>
+								<path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"></path>
 							</svg>
 						</span>
 					</a>
 					<a class="u-custom-color-3 u-gallery-nav u-gallery-nav-next u-icon-circle u-opacity u-opacity-70 u-spacing-10 u-text-white u-gallery-nav-2" href="#" role="button">
 						<span aria-hidden="true">
 							<svg viewBox="0 0 451.846 451.847">
-								<path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744
-L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284
-c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"></path>
+								<path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"></path>
 							</svg>
 						</span>
 						<span class="sr-only">
 							<svg viewBox="0 0 451.846 451.847">
-								<path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744
-L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284
-c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"></path>
+								<path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"></path>
 							</svg>
 						</span>
 					</a>
